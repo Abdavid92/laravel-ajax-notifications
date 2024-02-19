7dee38f1b6745f9ac00580376230ffb5b3384c40
Abel David <lleraabi@gmail.com>
2024-02-19 15:48:19 -0500

    Fix issues.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -70,7 +70,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function defineEnvironment($app): void
     {
-        $app['config']->set('ajaxnotifications.storage', 'file');
+        $app['config']->set('ajaxnotifications.storage', 'session');
         $app['config']->set('ajaxnotifications.flash', false);
     }
 }
